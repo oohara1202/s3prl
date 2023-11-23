26ee246e8ffa81a06c02d04efca6947cd9569802
s_koha <pdae9pw2@s.okayama-u.ac.jp>
2023-11-23 11:42:35 +0900

    実行スクリプト修正

--- s3prl/emotion_libri.sh
+++ s3prl/emotion_libri.sh
@@ -1,5 +1,5 @@
 for test_fold in fold1 fold2 fold3 fold4 fold5;
 do
-    python3 run_downstream.py -n emotion_libri_$test_fold -m train -u hubert -d emotion_libri -c downstream/emotion_libri/config.yaml -o "config.downstream_expert.datarc.test_fold='$test_fold'"
+    python3 run_downstream.py -n emotion_libri_$test_fold -m train -u hubert -d emotion -c downstream/emotion/config.yaml -o "config.downstream_expert.datarc.test_fold='$test_fold'"
     python3 run_downstream.py -m evaluate -e result/downstream/emotion_libri_$test_fold/dev-best.ckpt
 done
