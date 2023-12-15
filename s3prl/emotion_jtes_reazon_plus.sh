9078f08cdb2e76fd54c0229fa825d2554afd3cec
s_koha <pdae9pw2@s.okayama-u.ac.jp>
2023-12-15 10:49:53 +0900

    delete

--- s3prl/emotion_jtes_reazon_plus.sh
+++
@@ -1,5 +0,0 @@
-for test_fold in fold1 fold2 fold3 fold4 fold5;
-do
-    python3 run_downstream.py -n emotion_jtes_reazon_plus_$test_fold -m train -u hf_hubert_custom -k rinna/japanese-hubert-base -d emotion_jtes -c downstream/emotion_jtes/config.yaml -o "config.downstream_expert.datarc.test_fold='$test_fold'"
-    python3 run_downstream.py -m evaluate -e result/downstream/emotion_jtes_reazon_plus_$test_fold/dev-best.ckpt
-done
